8c14b9b51fed881c29cdfadf920326c02000b995
rosencrantz96 <topgdvidsyb@naver.com>
2024-11-21 18:03:14 +0900

    Added Lambda function log message

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    return {
+    
+    logger.info(f"Received event: {json.dumps(event)}")
+    
+    response = {
         "statusCode": 200,
         "body": json.dumps("Hello, AWS Lambda!")
     }
+    
+    logger.info(f"Returning response: {json.dumps(response)}")
+    
+    return response
